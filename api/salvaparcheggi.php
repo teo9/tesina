428c34e1fa67fc22cc44d22beb0f8782475f44f0
matteo <matteo99moro@gmail.com>
2018-06-01 15:53:38 +0200

    sistemate web-request

--- api/salvaparcheggi.php
+++ api/salvaparcheggi.php
@@ -1,12 +1,17 @@
 <?php
 $connessione = mysqli_connect("localhost","root", "", "gestionale");
-$nome=$_GET["nome"];
-$x = $_GET["x"];
-$y = $_GET["y"];
-$w = $_GET["w"];
-$h = $_GET["h"];
-$queryInserimento = "insert INTO parcheggio (nome,x,y,w,h) VALUES ('".$nome."','".$x."','".$y."','".$w."','".$h."')";
-$query = mysqli_query($connessione,$queryInserimento);
+$p = json_decode($_POST['dati']);
+
+foreach( $p as $parcheggio )
+{
+    $pp = json_decode($parcheggio);
+    $nome = $parcheggio["nome"];
+    $x = $parcheggio['x'];
+    $y = $parcheggio['y'];
+    $queryInserimento = "insert INTO parcheggio (nome,x,y,w,h) VALUES ('".$nome."','".$x."','".$y."','".$_POST['w']."','".$_POST['h']."')";
+    $query = mysqli_query($connessione,$queryInserimento);
+}
+
 $x = $x + ($w / 4);
 $y = $y + ($h / 2);
 $response = array('stato' => "ok" , 'nome' => "$nome" , "x" => "$y" , "y" => "$y");
