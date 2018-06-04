ca77a14f82934057314b2cf4a9c08b307a687ec4
ASUS\Utente <mturcato11@gmail.com>
2018-06-04 08:45:28 +0200

    pallinaaaa percorsoo

--- pallinatesina/percorso/js/click.js
+++ pallinatesina/percorso/js/click.js
@@ -12,59 +12,57 @@ function linea(posi, punti) {
             if (posi.x > punti[i].x && posi.x < punti[i].x + 20 && posi.y < punti[i].y + 20 && posi.y > punti[i].y) {
                 if(contamelo%2!=0)
                 {
-                    console.log("cliccato un punto"); 
                     tieni.x=punti[i].x;
-                    tieni.y=punti[i].y
-                    console.log(tieni);
-                    
+                    tieni.y=punti[i].y;  
+                    contamelo++;                
                 }
                 else
-                {
-                    console.log(punti[i]);
+                {             
                     for(var j=0;j<passati.length-1;j++)
-                    {
-                        if(tieni.x==passati[j].x && tieni.y== passati[j].y  && punti[i].x == passati[j+1].x && punti[i].y == passati[j+1].y)
+                    {                       
+                        if((tieni.x==passati[j].x && tieni.y== passati[j].y && punti[i].x == passati[j+1].x && punti[i].y == passati[j+1].y) || (tieni.x==punti[i].x && tieni.y==punti[i].y))
                         {
                             fai=false;
+                            contamelo=1;
                         }
                     }
                     if(fai==true)
-                    {
-                    ctx.beginPath();
-                    ctx.lineWidth = "1";
-                    ctx.moveTo(tieni.x,tieni.y);
-                    ctx.lineTo(punti[i].x,punti[i].y);
-                    ctx.fill();
-                    ctx.stroke();
-                    punto=false;
+                    {                        
+                        ctx.beginPath();
+                        ctx.lineWidth = "1";
+                        ctx.moveTo(tieni.x,tieni.y);
+                        ctx.lineTo(punti[i].x,punti[i].y);
+                        ctx.fill();
+                        ctx.stroke();
+                        punto=false;
 
-                    ctx.lineWidth = "1";
-                    ctx.beginPath();
-                    ctx.fillStyle = "yellow";
-                    ctx.strokeStyle = 'purple';
-                    ctx.arc(tieni.x, tieni.y, 10, 0, 2 * Math.PI);
-                    ctx.fill();
-                    ctx.stroke();
+                        ctx.lineWidth = "1";
+                        ctx.beginPath();
+                        ctx.fillStyle = "blue";
+                        ctx.strokeStyle = 'purple';
+                        ctx.arc(tieni.x, tieni.y, 10, 0, 2 * Math.PI);
+                        ctx.fill();
+                        ctx.stroke();
 
-                    ctx.lineWidth = "1";
-                    ctx.beginPath();
-                    ctx.fillStyle = "yellow";
-                    ctx.strokeStyle = 'purple';
-                    ctx.arc(punti[i].x, punti[i].y, 10, 0, 2 * Math.PI);
-                    ctx.fill();
-                    ctx.stroke();
-                    passati.push(tieni);
-                    passati.push(punti[i]);
-                    
+                        ctx.lineWidth = "1";
+                        ctx.beginPath();
+                        ctx.fillStyle = "blue";
+                        ctx.strokeStyle = 'purple';
+                        ctx.arc(punti[i].x, punti[i].y, 10, 0, 2 * Math.PI);
+                        ctx.fill();
+                        ctx.stroke();
+                        passati.push(tieni);
+                        passati.push(punti[i]);
+                        contamelo=1;
                     } 
                     else
                     {
-                        console.log("già fatto!");
+                        console.log("NO NO");
                     }                   
                 }
             }
         }
-        contamelo++;
+        
 }
 function isInside(pos) {
     for (var i = 0; i < all.length; i++) {
