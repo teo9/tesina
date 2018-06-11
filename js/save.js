9295fdb79f4d490f4ba3754b4b9c899be066b88e
ASUS\Utente <mturcato11@gmail.com>
2018-06-11 11:39:36 +0200

    sis

--- js/save.js
+++ js/save.js
@@ -23,17 +23,31 @@ function save()
         xhttp.open("POST", "./api/salvaparcheggi.php", true);	
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send("dati=" + data );
-        
+        var faicosa=true;
 
         var entry = '[';
-        
         for(var j=0;j<entrata.length;j++)
         {
             var x = Math.ceil((entrata[j].x-offset.x)/25);
             var y = Math.ceil((entrata[j].y-offset.y)/25);
+            for(k=0;k<parcheggi.length;k++)
+            {
+                if(x>=parcheggi[k].x && x<=parcheggi[k].x+1)
+                {
+                    faicosa=false;
+                }
+            }
+            if(faicosa==true)
+            {
+           
             if(j!=0)
                 entry += ',';
             entry+= "{\"x\":\""+x+"\",\"y\":\""+y+"\"}";
+            }
+            else
+            {
+                console.log("Entrata sopra parcheggio");
+            }
                    
         }
         var sxhttp = new XMLHttpRequest();
